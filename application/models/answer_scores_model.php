b3e4ade7b80f8b1799784d4df882609da7b75907
Bo-Yuan Ding <hydra200238@gmail.com>
2014-03-10 15:23:12 +0800

    update share like and question

--- application/models/answer_scores_model.php
+++ application/models/answer_scores_model.php
@@ -7,12 +7,20 @@ class Answer_scores_model extends My_Model
 		parent::__construct();
 	}
 	
-	function get_answer_scores($field = '*' ,$where)
+	function get_answer_scores($field = '*' ,$where,$limit=25,$offset=0)
 	{		
+        $this->db->order_by('answer_scores_id','DESC');
+        $this->db->join('user','answer_scores.user_id = user.user_id');
 		$this->db->select($field);
-		return $this->db->where($where)->get('answer_scores');
+        
+		return $this->db->where($where)->get('answer_scores',$limit,$offset);
 	}
 	
+    function get_answer_scores_count($where)
+    {
+		return $this->db->where($where)->count_all_results('answer_scores');
+    }
+    
 	function insert_answer_scores($data)
 	{
 		return $this->db->insert('answer_scores',$data);
