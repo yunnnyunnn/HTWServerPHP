0dada57b7eaf12103eaacb68efa4fca187fcdb7d
WU, Cheng-An <smallwu168@gmail.com>
2014-03-17 18:35:37 +0800

    feedbackupdate

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '123456';
+$db['default']['password'] = '123';
 $db['default']['database'] = 'weather';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
